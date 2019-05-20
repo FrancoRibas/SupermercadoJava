8357c0d86b3166e763d367d7512304eca4ce0d25
FrancoRibas <fdrserviceyventas@gmail.com>
2019-05-20 18:12:28 -0300

    primer nivel

--- src/model/Carrito.java
+++ src/model/Carrito.java
@@ -1,5 +1,46 @@
 package model;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Carrito {
-
+	
+	private int idCarrito;
+	private LocalDate fecha;
+	private LocalTime hora;
+	private List<ItemCarrito> lstCarrito;
+	public Carrito(int idCarrito, LocalDate fecha, LocalTime hora, List<ItemCarrito> lstCarrito) {
+		super();
+		this.idCarrito = idCarrito;
+		this.fecha = fecha;
+		this.hora = hora;
+		this.lstCarrito = lstCarrito;
+	}
+	public int getIdCarrito() {
+		return idCarrito;
+	}
+	public void setIdCarrito(int idCarrito) {
+		this.idCarrito = idCarrito;
+	}
+	public LocalDate getFecha() {
+		return fecha;
+	}
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+	public LocalTime getHora() {
+		return hora;
+	}
+	public void setHora(LocalTime hora) {
+		this.hora = hora;
+	}
+	public List<ItemCarrito> getLstCarrito() {
+		return lstCarrito;
+	}
+	public void setLstCarrito(List<ItemCarrito> lstCarrito) {
+		this.lstCarrito = lstCarrito;
+	}
+	
+	
 }
--- src/model/Supermercado.java
+++ src/model/Supermercado.java
@@ -1,5 +1,20 @@
 package model;
 
 public class Supermercado {
+	
+	private Producto gondola;
 
+	public Supermercado(Producto gondola) {
+		super();
+		this.gondola = gondola;
+	}
+
+	public Producto getGondola() {
+		return gondola;
+	}
+
+	public void setGondola(Producto gondola) {
+		this.gondola = gondola;
+	}
+	
 }
